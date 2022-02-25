62b6aa2dfbd9cc3c6dc8199a641ac349f0d0dbd3
Aldair Meraz <aldairrdz@gmail.com>
2022-02-25 10:18:59 -0600

    first commit

---
+++ src/main/java/org/generation/creeptomonedasdb/CreeptomonedasdbApplication.java
@@ -0,0 +1,13 @@
+package org.generation.creeptomonedasdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreeptomonedasdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreeptomonedasdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/creeptomonedasdb/CreeptomonedasdbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.creeptomonedasdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreeptomonedasdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
